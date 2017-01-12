08c14b2e66f447eae4a5aa1a794836013febe79e
Kenta Sato <bicycle1885@gmail.com>
2017-01-13 07:48:35 +0900

    add more tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -463,6 +463,31 @@ module Test10
     const re = Automa.RegExp
     using Base.Test
 
+    machine = Automa.compile(re.primitive(0x61))
+    @test Automa.execute(machine, "a")[1] == 0
+    @test Automa.execute(machine, "b")[1] < 0
+
+    machine = Automa.compile(re.primitive(0x61:0x62))
+    @test Automa.execute(machine, "a")[1] == 0
+    @test Automa.execute(machine, "b")[1] == 0
+    @test Automa.execute(machine, "c")[1] < 0
+
+    machine = Automa.compile(re.primitive('a'))
+    @test Automa.execute(machine, "a")[1] == 0
+    @test Automa.execute(machine, "b")[1] < 0
+
+    machine = Automa.compile(re.primitive('樹'))
+    @test Automa.execute(machine, "樹")[1] == 0
+    @test Automa.execute(machine, "儒")[1] < 0
+
+    machine = Automa.compile(re.primitive("ジュリア"))
+    @test Automa.execute(machine, "ジュリア")[1] == 0
+    @test Automa.execute(machine, "パイソン")[1] < 0
+
+    machine = Automa.compile(re.primitive([0x61, 0x62, 0x72]))
+    @test Automa.execute(machine, "abr")[1] == 0
+    @test Automa.execute(machine, "acr")[1] < 0
+
     machine = Automa.compile(re"[^A-Z]")
     @test Automa.execute(machine, "1")[1] == 0
     @test Automa.execute(machine, "A")[1] < 0
