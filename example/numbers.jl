85b99163533edb1a7c7ac354d085aa6f2dbbe0de
Kenta Sato <bicycle1885@gmail.com>
2017-01-18 18:23:04 +0900

    fix numbers.jl

--- example/numbers.jl
+++ example/numbers.jl
@@ -1,37 +1,39 @@
+# A tokenizer of octal, decimal, hexadecimal and floating point numbers
+# =====================================================================
+
 import Automa
 import Automa.RegExp: @re_str
 const re = Automa.RegExp
 
-# Describe regular expression patterns.
+# Describe patterns in regular expression.
+oct      = re"0o[0-7]+"
 dec      = re"[-+]?[0-9]+"
 hex      = re"0x[0-9A-Fa-f]+"
-oct      = re"0o[0-7]+"
 prefloat = re"[-+]?([0-9]+\.[0-9]*|[0-9]*\.[0-9]+)"
 float    = prefloat | re.cat(prefloat | re"[-+]?[0-9]+", re"[eE][-+]?[0-9]+")
-number   = dec | hex | oct | float
+number   = oct | dec | hex | float
 numbers  = re.cat(re.opt(number), re.rep(re" +" * number), re" *")
 
 # Register action names to regular expressions.
 number.actions[:enter] = [:mark]
+oct.actions[:exit]     = [:oct]
 dec.actions[:exit]     = [:dec]
 hex.actions[:exit]     = [:hex]
-oct.actions[:exit]     = [:oct]
 float.actions[:exit]   = [:float]
 
 # Compile a finite-state machine.
 machine = Automa.compile(numbers)
 
-#= This generates a SVG file to visualize the state machine.
-write("numbers.dot", Automa.dfa2dot(machine.dfa))
-run(`dot -Tpng -o numbers.png numbers.dot`)
-=#
+# This generates a SVG file to visualize the state machine.
+# write("numbers.dot", Automa.dfa2dot(machine.dfa))
+# run(`dot -Tpng -o numbers.png numbers.dot`)
 
 # Bind an action code for each action name.
 actions = Dict(
     :mark  => :(mark = p),
+    :oct   => :(emit(:oct)),
     :dec   => :(emit(:dec)),
     :hex   => :(emit(:hex)),
-    :oct   => :(emit(:oct)),
     :float => :(emit(:float)),
 )
 
