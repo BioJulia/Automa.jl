90a97de7d301ec9a7e77cc4d52091cdab7a4eb60
Kenta Sato <bicycle1885@gmail.com>
2017-02-25 17:58:59 +0900

    test :all

--- test/runtests.jl
+++ test/runtests.jl
@@ -662,6 +662,31 @@ module Test11
     @test validate2(b"1\n", 1) == ([], :error)
 end
 
+module Test12
+    import Automa
+    import Automa.RegExp: @re_str
+    const re = Automa.RegExp
+    using Base.Test
+
+    a = re"a*"
+    a.actions[:all] = [:a]
+    machine = Automa.compile(a)
+
+    @eval function validate(data)
+        logger = Symbol[]
+        $(Automa.generate_init_code(machine))
+        p_end = p_eof = sizeof(data)
+        $(Automa.generate_exec_code(machine, actions=:debug))
+        return logger, cs == 0 ? :ok : cs < 0 ? :error : :incomplete
+    end
+
+    @test validate(b"") == ([], :ok)
+    @test validate(b"a") == ([:a], :ok)
+    @test validate(b"aa") == ([:a, :a], :ok)
+    @test validate(b"aaa") == ([:a, :a, :a], :ok)
+    @test validate(b"aaab") == ([:a, :a, :a], :error)
+end
+
 module TestFASTA
     include("../example/fasta.jl")
     using Base.Test
