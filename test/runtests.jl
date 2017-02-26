8d1744672d7460d069e0a1f200145b329fa87481
Kenta Sato <bicycle1885@gmail.com>
2017-02-26 16:19:35 +0900

    fix off-by-one position problem of the goto codegen

--- test/runtests.jl
+++ test/runtests.jl
@@ -715,6 +715,39 @@ module Test13
     @test validate(b"abrad") == ([], :error)
 end
 
+module Test14
+    import Automa
+    import Automa.RegExp: @re_str
+    const re = Automa.RegExp
+    using Base.Test
+
+    a = re"a*"
+    machine = Automa.compile(a)
+    @eval function validate_table(data)
+        $(Automa.generate_init_code(machine))
+        p_end = p_eof = sizeof(data)
+        $(Automa.generate_exec_code(machine, code=:table))
+        return p, cs
+    end
+    @eval function validate_inline(data)
+        $(Automa.generate_init_code(machine))
+        p_end = p_eof = sizeof(data)
+        $(Automa.generate_exec_code(machine, code=:inline))
+        return p, cs
+    end
+    @eval function validate_goto(data)
+        $(Automa.generate_init_code(machine))
+        p_end = p_eof = sizeof(data)
+        $(Automa.generate_exec_code(machine, code=:goto))
+        return p, cs
+    end
+
+    @test validate_table(b"")   == validate_inline(b"")   == validate_goto(b"")
+    @test validate_table(b"a")  == validate_inline(b"a")  == validate_goto(b"a")
+    @test validate_table(b"b")  == validate_inline(b"b")  == validate_goto(b"b")
+    @test validate_table(b"ab") == validate_inline(b"ab") == validate_goto(b"ab")
+end
+
 module TestDOT
     import Automa
     import Automa.RegExp: @re_str
