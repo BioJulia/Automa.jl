62e90d272bac3e7a60a60ab73842a51f7736190c
Kenta Sato <bicycle1885@gmail.com>
2017-02-27 00:18:15 +0900

    fixup! make action ordering more intuitive

--- src/edge.jl
+++ src/edge.jl
@@ -11,10 +11,6 @@ function Edge(labels::ByteSet)
     return Edge(labels, Set{Precondition}(), ActionList())
 end
 
-#function Edge(labels::ByteSet, actions::Union{Set{Action},Vector{Action}})
-#    return Edge(labels, Set{Precondition}(), ActionList(actions))
-#end
-
 function Edge(labels::ByteSet, actions::ActionList)
     return Edge(labels, Set{Precondition}(), actions)
 end
