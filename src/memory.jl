038fb8307b85f02370cc809123aa0bdcd023c6d4
Kenta Sato <bicycle1885@gmail.com>
2017-07-10 17:57:44 +0900

    introduce pointerstart and pointerend

--- src/memory.jl
+++ src/memory.jl
@@ -11,17 +11,35 @@ end
 
 Create a `SizedMemory` object from `data`.
 
-`data` must implement `Base.pointer` and `Base.sizeof` methods.  For example,
-`String` and `Vector{UInt8}` support these methods. Note that it is user's
-responsibility to keep the `data` object alive during `SizedMemory`'s lifetime
-because it does not have a reference to the object.
+`data` must implement `Base.pointer` and `Base.sizeof` (or `Automa.pointerend`)
+methods.  For example, `String` and `Vector{UInt8}` support these methods. Note
+that it is user's responsibility to keep the `data` object alive during
+`SizedMemory`'s lifetime because it does not have a reference to the object.
 """
-function SizedMemory(data, len::Integer=(pointerend(data) + 1) - pointer(data))
-    return SizedMemory(pointer(data), len)
+function SizedMemory(data, len::Integer=(pointerend(data) + 1) - pointerstart(data))
+    return SizedMemory(pointerstart(data), len)
 end
 
-function pointerend(data)
-    return pointer(data, sizeof(data))
+"""
+    pointerstart(data)::Ptr{UInt8}
+
+Return the start position of `data`.
+
+The default implementation is `convert(Ptr{UInt8}, pointer(data))`.
+"""
+function pointerstart(data)::Ptr{UInt8}
+    return convert(Ptr{UInt8}, pointer(data))
+end
+
+"""
+    pointerend(data)::Ptr{UInt8}
+
+Return the end position of `data`.
+
+The default implementation is `Automa.pointerstart(data) + sizeof(data) - 1`.
+"""
+function pointerend(data)::Ptr{UInt8}
+    return pointerstart(data) + sizeof(data) - 1
 end
 
 function Base.checkbounds(mem::SizedMemory, i::Integer)
