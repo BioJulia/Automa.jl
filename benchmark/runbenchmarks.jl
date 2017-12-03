138f9e48d536e05db3e67f5f048c29ed74ae8a20
Kenta Sato <bicycle1885@gmail.com>
2017-12-03 23:01:25 +0900

    add a benchmark

--- benchmark/runbenchmarks.jl
+++ benchmark/runbenchmarks.jl
@@ -19,10 +19,10 @@ end
 # Case 1
 # ------
 
-println(raw"([A-Za-z]*\r?\n)*")
-println("PCRE:                 ", @benchmark ismatch($(r"^(:?[A-Za-z]*\r?\n)*$"), data))
+println("Case 1 ", raw"([A-z]*\r?\n)*")
+println("PCRE:                 ", @benchmark ismatch($(r"^(:?[A-z]*\r?\n)*$"), data))
 
-machine = Automa.compile(re"([A-Za-z]*\r?\n)*")
+machine = Automa.compile(re"([A-z]*\r?\n)*")
 VISUALIZE && writesvg("case1", machine)
 context = Automa.CodeGenContext(generator=:goto, checkbounds=false)
 @eval function match(data)
@@ -47,11 +47,39 @@ println("Automa.jl (unrolled): ", @benchmark match(data))
 # ------
 
 println()
-println(raw"([ACGTacgt]*\r?\n)*")
+println("Case 2 ", raw"([A-Za-z]*\r?\n)*")
+println("PCRE:                 ", @benchmark ismatch($(r"^(:?[A-Za-z]*\r?\n)*$"), data))
+
+machine = Automa.compile(re"([A-Za-z]*\r?\n)*")
+VISUALIZE && writesvg("case2", machine)
+context = Automa.CodeGenContext(generator=:goto, checkbounds=false)
+@eval function match(data)
+    $(Automa.generate_init_code(context, machine))
+    p_end = p_eof = endof(data)
+    $(Automa.generate_exec_code(context, machine))
+    return cs == 0
+end
+println("Automa.jl:            ", @benchmark match(data))
+
+context = Automa.CodeGenContext(generator=:goto, checkbounds=false, loopunroll=10)
+@eval function match(data)
+    $(Automa.generate_init_code(context, machine))
+    p_end = p_eof = endof(data)
+    $(Automa.generate_exec_code(context, machine))
+    return cs == 0
+end
+println("Automa.jl (unrolled): ", @benchmark match(data))
+
+
+# Case 3
+# ------
+
+println()
+println("Case 3 ", raw"([ACGTacgt]*\r?\n)*")
 println("PCRE:                 ", @benchmark ismatch($(r"^(:?[ACGTacgt]*\r?\n)*$"), data))
 
 machine = Automa.compile(re"([ACGTacgt]*\r?\n)*")
-VISUALIZE && writesvg("case2", machine)
+VISUALIZE && writesvg("case3", machine)
 context = Automa.CodeGenContext(generator=:goto, checkbounds=false)
 @eval function match(data)
     $(Automa.generate_init_code(context, machine))
