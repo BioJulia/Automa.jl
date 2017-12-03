ff329ed4dfeeda788088a2dbd32cbcc974dc2f13
Kenta Sato <bicycle1885@gmail.com>
2017-12-03 22:45:40 +0900

    write SVG when benchmarking

--- benchmark/runbenchmarks.jl
+++ benchmark/runbenchmarks.jl
@@ -5,6 +5,16 @@ using BenchmarkTools
 srand(1234)
 data = String(vcat([push!(rand(b"ACGTacgt", 59), UInt8('\n')) for _ in 1:1000]...))
 
+VISUALIZE = false
+function writesvg(name, machine)
+    dot = joinpath(@__DIR__, "$(name).dot")
+    svg = joinpath(@__DIR__, "$(name).svg")
+    info("writing $(dot)")
+    write(dot, Automa.machine2dot(machine))
+    info("writing $(svg)")
+    run(`dot -Tsvg -o $(svg) $(dot)`)
+end
+
 
 # Case 1
 # ------
@@ -13,6 +23,7 @@ println(raw"([A-Za-z]*\r?\n)*")
 println("PCRE:                 ", @benchmark ismatch($(r"^(:?[A-Za-z]*\r?\n)*$"), data))
 
 machine = Automa.compile(re"([A-Za-z]*\r?\n)*")
+VISUALIZE && writesvg("case1", machine)
 context = Automa.CodeGenContext(generator=:goto, checkbounds=false)
 @eval function match(data)
     $(Automa.generate_init_code(context, machine))
@@ -40,6 +51,7 @@ println(raw"([ACGTacgt]*\r?\n)*")
 println("PCRE:                 ", @benchmark ismatch($(r"^(:?[ACGTacgt]*\r?\n)*$"), data))
 
 machine = Automa.compile(re"([ACGTacgt]*\r?\n)*")
+VISUALIZE && writesvg("case2", machine)
 context = Automa.CodeGenContext(generator=:goto, checkbounds=false)
 @eval function match(data)
     $(Automa.generate_init_code(context, machine))
