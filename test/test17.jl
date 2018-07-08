14dd6a256b144b282567bc48c3735a8eb7fba37f
Daniel C. Jones <dcjones@cs.washington.edu>
2018-07-08 15:33:22 -0700

    Fix escaping - and ^ inside character classes.

---
+++ test/test17.jl
@@ -0,0 +1,58 @@
+module Test17
+
+if VERSION >= v"0.7-"
+    using Test
+else
+    using Base.Test
+end
+import Automa
+import Automa.RegExp: @re_str
+import Compat: lastindex, occursin
+
+@testset "Test17" begin
+    re1 = re"[a\-c]"
+    re1.actions[:enter] = [:enter]
+    re1.actions[:exit] = [:exit]
+    machine1 = Automa.compile(re1)
+    @test occursin(r"^Automa.Machine\(<.*>\)$", repr(machine1))
+
+    for generator in (:table, :inline, :goto), checkbounds in (true, false), clean in (true, false)
+        ctx = Automa.CodeGenContext(generator=generator, checkbounds=checkbounds, clean=clean)
+        init_code = Automa.generate_init_code(ctx, machine1)
+        exec_code = Automa.generate_exec_code(ctx, machine1, :debug)
+        validate = @eval function (data)
+            logger = Symbol[]
+            $(init_code)
+            p_end = p_eof = lastindex(data)
+            $(exec_code)
+            return cs == 0, logger
+        end
+
+        @test validate(b"-") == (true, [:enter, :exit])
+        @test validate(b"b") == (false, Symbol[])
+    end
+
+    re2 = re"[a-c]"
+    re2.actions[:enter] = [:enter]
+    re2.actions[:exit] = [:exit]
+    machine2 = Automa.compile(re2)
+    @test occursin(r"^Automa.Machine\(<.*>\)$", repr(machine2))
+
+    for generator in (:table, :inline, :goto), checkbounds in (true, false), clean in (true, false)
+        ctx = Automa.CodeGenContext(generator=generator, checkbounds=checkbounds, clean=clean)
+        init_code = Automa.generate_init_code(ctx, machine2)
+        exec_code = Automa.generate_exec_code(ctx, machine2, :debug)
+        validate = @eval function (data)
+            logger = Symbol[]
+            $(init_code)
+            p_end = p_eof = lastindex(data)
+            $(exec_code)
+            return cs == 0, logger
+        end
+
+        @test validate(b"-") == (false, [])
+        @test validate(b"b") == (true, Symbol[:enter, :exit])
+    end
+end
+
+end
