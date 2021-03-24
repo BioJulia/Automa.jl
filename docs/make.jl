fb36cc0db960bd690fb34afa75736bc2ac47d09e
Kevin Bonham <kevbonham@gmail.com>
2021-03-24 10:28:07 -0400

    update docs make and github action

--- docs/make.jl
+++ docs/make.jl
@@ -5,19 +5,18 @@ using Automa
 # run(`julia preconditions.jl`)
 
 makedocs(
-    format = :html,
     sitename = "Automa.jl",
     modules = [Automa],
     pages = [
         "Home" => "index.md",
         "References" => "references.md"
-    ]
+        ],
+    format = Documenter.HTML(
+        prettyurls = get(ENV, "CI", nothing) == "true")
 )
 
 deploydocs(
     repo = "github.com/BioJulia/Automa.jl.git",
-    julia = "0.6",
     target = "build",
-    deps = nothing,
-    make = nothing
+    push_preview = true,  
 )
