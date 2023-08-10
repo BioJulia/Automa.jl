7ddb042d40e63041aa148b2e908c0ed62807d28b
Jakob Nybo Nissen <jakobnybonissen@gmail.com>
2023-08-10 12:24:58 +0200

    Backport from v1: Remove SIMD capability
    I've come to the conclusion that Julia does not make it possible to robustly
    check what CPU instructions the user has available.
    The current options are all undocumented, complex and brittle, and not suitable
    for code that cannot be accepted to break at any time
    
    Whenever a robust way of checking for CPU instructions are available, the change
    is easy to revert.

--- src/Automa.jl
+++ src/Automa.jl
@@ -1,28 +1,6 @@
 module Automa
 
-import ScanByte: ScanByte, ByteSet
-
-# Encode a byte set into a sequence of non-empty ranges.
-function range_encode(set::ScanByte.ByteSet)
-    result = UnitRange{UInt8}[]
-    it = iterate(set)
-    it === nothing && return result
-    start, state = it
-    lastbyte = byte = start
-    it = iterate(set)
-    while it !== nothing
-        byte, state = it
-        if byte > lastbyte + 1
-            push!(result, start:lastbyte)
-            start = byte
-        end
-        lastbyte = byte
-        it = iterate(set, state)
-    end
-    push!(result, start:byte)
-    return result
-end
-
+include("byteset.jl")
 include("re.jl")
 include("precond.jl")
 include("action.jl")
